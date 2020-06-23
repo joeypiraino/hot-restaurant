f5af00c0772a110742449a38e69eacea7fb447de
Big Sic <joeypiraino@gmail.com>
2020-06-23 03:01:40 -0500

    update to scripts

--- server.js
+++ server.js
@@ -36,11 +36,11 @@ app.post("/api/tables", function (req, res) {
 
 	// Using a RegEx Pattern to remove spaces from newTable
 	// You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-	newTable.routeName = newTable.name.replace(/\s+/g, "").toLowerCase();
+	newTable.routeName = newTable.name.replace(/\s+/g, "");
 
 	console.log(newTable);
 
-	characters.push(newTable);
+	tables.push(newTable);
 
 	res.json(newTable);
 });
