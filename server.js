f8ce24cb799b2630e37f7159a4acb83bdbcb91e6
Big Sic <joeypiraino@gmail.com>
2020-06-22 21:45:24 -0500

    filepath updates

--- server.js
+++ server.js
@@ -12,40 +12,43 @@ app.use(express.json());
 var tables = ["Table 1", "Table 2", "Table 3", "Table 4", "Table 5"];
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "view.html"));
+	res.sendFile(path.join(__dirname, "view.html"));
 });
 
+app.get("/tables", function (req, res) {
+	res.sendFile(path.join(__dirname, "resviews.html"));
+});
 // app.get("/makeres", function (req, res) {
 //   res.sendFile(path.join(__dirname, "form.html"));
 // });
 
 app.get("/add", function (req, res) {
-  console.log(req);
-  res.sendFile(path.join(__dirname, "form.html"));
+	console.log(req);
+	res.sendFile(path.join(__dirname, "form.html"));
 });
 
 app.get("/api/tables", function (req, res) {
-  return res.json(tables);
+	return res.json(tables);
 });
 
 // Create New Tables - takes in JSON input
 app.post("/api/tables", function (req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
-  var newTable = req.body;
+	// req.body hosts is equal to the JSON post sent from the user
+	// This works because of our body parsing middleware
+	var newTable = req.body;
 
-  // Using a RegEx Pattern to remove spaces from newTable
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newTable.routeName = newTable.name.replace(/\s+/g, "").toLowerCase();
+	// Using a RegEx Pattern to remove spaces from newTable
+	// You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+	newTable.routeName = newTable.name.replace(/\s+/g, "").toLowerCase();
 
-  console.log(newTable);
+	console.log(newTable);
 
-  characters.push(newTable);
+	characters.push(newTable);
 
-  res.json(newTable);
+	res.json(newTable);
 });
 
 /// server listening
 app.listen(PORT, function () {
-  console.log("App listening on PORT " + PORT);
+	console.log("App listening on PORT " + PORT);
 });
